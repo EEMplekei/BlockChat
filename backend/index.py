888bd56327c9eada5e1063ac123cc05a4d7bd7c7
George Sotiropoulos <gsotiropoulos@msi.gr>
2024-02-25 15:37:23 +0200

    Delete backend index.py file

--- backend/index.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-# Create an instance of the FastAPI class
-app = FastAPI()
-
-# Define a route using a decorator
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World"}
