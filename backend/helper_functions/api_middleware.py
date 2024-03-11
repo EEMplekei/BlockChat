b07bf987abc3a6b4ec970267784f1498af0c87cb
Node1 <you@example.com>
2024-03-11 20:30:27 +0000

    final route restriction commit

--- backend/helper_functions/api_middleware.py
+++ backend/helper_functions/api_middleware.py
@@ -13,25 +13,25 @@ import time
 # HACK: This is a hack to avoid circular imports, use https://docs.python.org/3/library/ipaddress.html
 
 try:
-	INTERNAL_CIDR = try_load_env('INTERNAL_CIDR')
-	internal_network = ip_network(INTERNAL_CIDR)
+    INTERNAL_CIDR = try_load_env('INTERNAL_CIDR')
+    internal_network = ip_network(INTERNAL_CIDR)
 except ValueError:
-	print(f"{Fore.YELLOW}restrict_internal_routes{Fore.RESET}: {Fore.RED}Invalid CIDR range: {INTERNAL_CIDR}{Fore.RESET}")
-	exit()
+    print(f"{Fore.YELLOW}restrict_internal_routes{Fore.RESET}: {Fore.RED}Invalid CIDR range: {INTERNAL_CIDR}{Fore.RESET}")
+    exit()
 
 #Middleware to restrict access to internal routes
 async def restrict_internal_routes(request: Request, call_next: Callable) -> Response:
-	client_ip = ip_address(request.client.host)
+    client_ip = ip_address(request.client.host)
+    if not client_ip.is_loopback and client_ip not in internal_network:
+        return JSONResponse({'error': f'Access denied for IP {client_ip}'}, status_code=status.HTTP_400_BAD_REQUEST)
 
-	if not client_ip.is_loopback and client_ip in internal_network:
-		return JSONResponse({'error': f'Access denied for IP {client_ip}'}, status_code=status.HTTP_400_BAD_REQUEST)
-
-	return await call_next(request)
+    return await call_next(request)
 
 #Middleware to add process time header
 async def add_process_time_header(request: Request, call_next: Callable) -> Response:
-	start_time = time.time()
-	response = await call_next(request)
-	process_time = time.time() - start_time
-	response.headers["X-Process-Time"] = str(process_time)
-	return response
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
