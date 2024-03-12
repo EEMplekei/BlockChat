581ef982dc70699ae7298fda48935bb6a3bca552
Panagiotis Stefanis <panstef5555@gmail.com>
2024-03-12 14:09:37 +0200

    changed controllers to routes

--- backend/api.py
+++ backend/api.py
@@ -4,9 +4,9 @@ import uvicorn
 
 try:
 	from helper_functions.middleware import restrict_internal_routes
-	from controllers.internal_api import internal_api
-	from controllers.public_api import public_api
-	from controllers.shared_recourses import node
+	from routes.internal_api import internal_api
+	from routes.public_api import public_api
+	from routes.shared_recourses import node
 except ImportError as e:
 	print(f"{Fore.RED}Could not import required classes: {e}{Fore.RESET}")
 	exit()
