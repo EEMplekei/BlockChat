76e9f9e2acf7bdb04322b7e362a33903a44d3bac
George Sotiropoulos <gsotiropoulos@msi.gr>
2024-03-14 16:59:24 +0200

    parse_input tested

--- testing/helper_functions/parse_input.py
+++ testing/helper_functions/parse_input.py
@@ -17,13 +17,13 @@ def parse_input_files(node_number: int):
 					receiver_id.append(current_receiver_id)
 
 					# Debugging
-					print(f"Transaction id: {receiver_id}")
+					print(f"Transaction id: {current_receiver_id}")
 					# Get message after id and one space after
 					current_message = line[match.end():].lstrip()  # Remove the first space from the message
 					message.append(current_message)
 
 					# Debugging
-					print(f"Message: {message}")
+					print(f"Message: {current_message}")
 			except re.error as e:
 					# Handle exceptions
 					print("❌ Parsing Failed ❌")
