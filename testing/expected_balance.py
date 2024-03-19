bc571fd4d5b7c67b5b9855701ce48eed3fe6e05c
George Sotiropoulos <gsotiropoulos@msi.gr>
2024-03-19 21:47:25 +0200

    temp_balance expected

--- testing/expected_balance.py
+++ testing/expected_balance.py
@@ -9,26 +9,25 @@ def expected_balance(number_of_nodes, node):
     else: 
         print("Invalid")
         return
-    
     balance = 1000
-
     receiver_id_list, messages_list = parse_input.parse_input_files(trans_folder)
     for message in messages_list:
         balance -= len(message)
-
+    # Due to staking 10 at the beginning (we are talking about temp_balance, not balance)
+    balance -= 10
     return balance
 
-print(f"{Fore.GREEN}Expected Balances for 5 NODES{Fore.RESET}")
+print(f"{Fore.GREEN}Expected Temp_Balances for 5 NODES{Fore.RESET}")
 print()
 
 for i in range(5):
-    print(f"Node {i} Expected balance: {expected_balance(5, i)}")
+    print(f"Node {i} Expected Temp_Balance: {expected_balance(5, i)}")
 
 
 print()
 print("==============================================")
-print(f"{Fore.GREEN}Expected Balances for 10 NODES{Fore.RESET}")
+print(f"{Fore.GREEN}Expected Temp_Balances for 10 NODES{Fore.RESET}")
 print()
 
 for i in range(10):
-    print(f"Node {i} Expected balance: {expected_balance(10, i)}")
+    print(f"Node {i} Expected Temp_Balance: {expected_balance(10, i)}")
